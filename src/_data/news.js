864db46bd9dd5b8858c048666ea4f10f9c6769c4
Marek Valko <valko.marek@gmail.com>
2020-03-14 15:44:35 +0100

    Adding dynamic data on build

---
+++ src/_data/news.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+require('dotenv').config()
+
+const API_KEY = process.env.API_KEY
+
+const url = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}&pageSize=5`
+
+module.exports = async function() {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
